251b2801d52ff35caf1642b798eb31e72f46bd6c
Andrey Kiselev <akiselev@pushwoosh.com>
2022-03-15 13:22:47 +0100

    release 6.1.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PushwooshInboxUI",
-            url: "https://github.com/Pushwoosh/pushwoosh-inbox-ui-ios-sdk/releases/download/6.1.0/PushwooshInboxUI.xcframework.zip",
-            checksum: "2236e828e6fcaf5591d8368a584e93356985f8913da179e531c72fd157658a5f"
+            url: "https://github.com/Pushwoosh/pushwoosh-inbox-ui-ios-sdk/releases/download/6.1.1/PushwooshInboxUI.xcframework.zip",
+            checksum: "d75c026417f7a683fc5925459eadd7e99126a324d4edc66c663be66ef2cb8306"
         )
     ]
 )
