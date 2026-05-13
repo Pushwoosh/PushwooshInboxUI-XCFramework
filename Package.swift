50278d4740b98e02bbf10ada38ba83cf8cc751d0
pushwoosh <pushwoosh@PushwoosMini001.ovb>
2026-05-14 01:26:31 +0700

    Release 7.0.41

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PushwooshInboxUI",
-            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/7.0.40/PushwooshInboxUI.xcframework.zip",
-            checksum: "6826c2899cda31f515f4757e3702e310252f5f8aaa0d11e7d851c2d641051c5a"
+            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/7.0.41/PushwooshInboxUI.xcframework.zip",
+            checksum: "335d6331b5897817074b1a23b848aaebaceeb52b2411720992feb1d7586853ad"
         )
     ]
 )
